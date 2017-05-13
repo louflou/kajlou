878831b42c0beee8c511c87a5099f80dd2498854
Lou <louise@flou.eu>
2017-05-13 13:39:18 +0200

    Fix

--- 2.0/vitvarorDB.sql
+++ 2.0/vitvarorDB.sql
@@ -15,7 +15,6 @@ create table customers(
 	region text not null,
 	primary key(pno)
 );
-
 create table products(
 	product_id serial unique not null,
 	product_name text not null,
@@ -27,7 +26,6 @@ create table products(
 	active boolean not null,
 	primary key(product_id)	
 );
-
 create table staff(
 	staff_id serial unique not null,
 	title text not null,
@@ -36,14 +34,12 @@ create table staff(
 	email text not null,
 	primary key(staff_id)
 );
-
 create table supplier(
 	supplier_name text not null,
 	phone text not null,
 	website text not null,
 	primary key(supplier_name)
 );
-
 create table inventory(
 	product_id integer not null references products,
 	supplier text not null references supplier,
@@ -51,43 +47,36 @@ create table inventory(
 	quantity integer not null,
 	primary key(product_id, supplier)
 );
-
 create table sales_details(
-	sales_id integer not null,
+	sales_id serial unique not null,
 	customer_id varchar not null references customers,
 	staff_id integer not null references staff,
-	date integer not null,
+	date date not null,
 	discount integer not null,
-	subtotal integer not null,
+	subtotal integer,
 	primary key(sales_id)
 );
-
 create table sales(
 	sales_id integer not null references sales_details,
 	product_id integer not null references products,
 	quantity integer not null,
 	primary key(sales_id, product_id)
 );
-
 insert into staff (title, staff_name, phone, email) values 
 	('Manager', 'Louise Flou', '076-045 94 48', 'louise@flou.eu'),
 	('Manager', 'Kajsa Araskoug', '076-207 22 48', 'kajsa.araskoug@gmail.com'),
 	('Vendor', 'Henry Ford', '075-231 90 00', 'henry@mortfors-vv.com');
-
 insert into customers (pno, customer_name, email, address, postno, region) values
 	('880924-1723', 'Mikaela Arvidsson', 'mikkan__88@hotmail.com', 'Bäckahästgatan 35', '234 12', 'Malmö'),
 	('791103-4551', 'Arvid Svensson', 'arvid.p.svensson@hotmail.com', 'Solbackevägen 7A', '213 13', 'Svalöv'),
 	('910127-7840', 'Hanna Svärdh', 'svärdhiz_91@hotmail.com', 'Viskaregatan 26F', '253 60', 'Ramlösa');
-	
-
 insert into supplier(supplier_name, phone, website) values
 	('Candy', '040-141202', 'http://www.candy-domestic.co.uk/'),
 	('Elektrolux', '08-141614', 'http://www.electrolux.se/'),
 	('WhiteAway', '040-139000', 'http://www.whiteaway.se/'),
 	('Bosch', '040-120220', 'http://www.bosch.se/'),
-	('Smeg', '045-340211', 'http://www.smeg.se/');
-	
-	
+	('Smeg', '045-340211', 'http://www.smeg.se/'),
+	('Miele', '040-129054', 'https://www.miele.se/');
 insert into products (product_name, description, brand, price, category, image, active) values
 	('Candy Tvättmaskin', 'Utrusta ditt hushåll med denna kraftfulla tvättmaskin från Candy och njut av kvalitativa tvättresultat.', 'Candy', 2990, 'tvättmaskin', 'https://tubby.scene7.com/is/image/tubby/CS1692D3?$prod_all4one$', true),
 	
@@ -99,19 +88,30 @@ insert into products (product_name, description, brand, price, category, image, 
 	
 	('Smeg Colonial Spis', '60 cm bred gasspis i retrodesign som kommer att förgylla ditt kök. Energiklass A.', 'Smeg', 15995, 'spis', 'https://tubby.scene7.com/is/image/tubby/CO68GMA8?$prod_all4one$', true),
 	
-	('Smeg diskmaskin (svart)', 'Diskmaskin med plats till 13 kuvert i tuff retrodesign från Smeg. Energiklass A+++.', 'Smeg', 11690, 'diskmaskin', 'https://tubby.scene7.com/is/image/tubby/ST2FABNE2?$prod_all4one$', true);
+	('Smeg diskmaskin (svart)', 'Diskmaskin med plats till 13 kuvert i tuff retrodesign från Smeg. Energiklass A+++.', 'Smeg', 11690, 'diskmaskin', 'https://tubby.scene7.com/is/image/tubby/ST2FABNE2?$prod_all4one$', true),
+	
+	('Helintegrerad diskmaskin XXL', 'Höjdjusterbar överkorg, Standardprogram, t.ex. ECO, Automatic och Intensiv 75°C', 'Miele', 15300, 'diskmaskin', 'https://www.miele.se/pmedia/30/Z17/20000130901-000-00_20000130901.jpg', false),
+	
+	('Bosch Vario Perfect Tvättmaskin', 'Maskinen har en kapacitet på 8 kg. Centrifugeringshastighet är upp till 1400 varv per minut. Det är en kolfri motor i tvättmaskinen. Har den bästa energiklassen på marknaden: A+++.', 'Bosch', 7490, 'tvättmaskin', 'https://images.wagcdn.com/1200/1200/fill/p/prod_auto/frontbetjente-vaskemaskiner/wat286i8sn.jpg',  false);
 	
 insert into inventory (product_id, supplier, product_cost, quantity) values
 	(1, 'Candy', 1500 ,8),
 	(2, 'Elektrolux', 4000, 12),
-	(2, 'WhiteAway', 4620, 2),
-	(3, 'Bosch', 6350, 10),
-	(3, 'WhiteAway', 6500, 5),
-	(4, 'WhiteAway', 4495, 6),
-	(5, 'Smeg', 12345, 9),
-	(6, 'Smeg', 9995, 6);
+	(3, 'WhiteAway', 4620, 2),
+	(4, 'Bosch', 6350, 10),
+	(5, 'WhiteAway', 6500, 5),
+	(6, 'WhiteAway', 4495, 6),
+	(7, 'Smeg', 12345, 9),
+	(8, 'Smeg', 9995, 6),
+	(9, 'Miele', 12400, 0),
+	(10, 'Bosch', 5590, 2);
 
+insert into sales_details(customer_id, staff_id, date, discount) values 
+	('791103-4551', 3, '2017-05-11', 0);
 
+insert into sales(sales_id, product_id, quantity) values
+	(1, 5, 1);
+	
+select * from inventory;
 	
 
-	
