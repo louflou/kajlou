3716a49022df87404429ef050bcc7933456fee23
Lou <louise@flou.eu>
2017-05-22 09:04:46 +0200

    söndags-sync

--- admin/main.py
+++ admin/main.py
@@ -4,7 +4,7 @@ import bottle
 from bottle import route, run, template, os, static_file, debug, request, redirect
 
 #Konnectar till databsen
-conn = psycopg2.connect(dbname="ag8789", user="ag8789", password="cl934pos", host="pgserver.mah.se")
+conn = psycopg2.connect(dbname="kajlou", user="ag8789", password="cl934pos", host="pgserver.mah.se")
 
 #Pekaren på databasen
 cursor = conn.cursor()
@@ -60,6 +60,37 @@ def add_customer():
     conn.commit()
     redirect("/customers")
 
+@route("/add_supplier", method="POST")
+def add_customer():
+    supplier_name = str(request.forms.get("supplier_name"))
+    phone = str(request.forms.get("supplier_phone"))
+    website = str(request.forms.get("website"))
+    cursor.execute("INSERT INTO supplier (supplier_name, phone, website) values(%s, %s, %s)", (supplier_name, phone, website))
+    conn.commit()
+    redirect("/suppliers")
+
+
+@route("/add_product", method="POST")
+def add_product():
+    product_name = str(request.forms.get("product_name"))
+    description = str(request.forms.get("description"))
+    category = str(request.forms.get("category"))
+    image = str(request.forms.get("product_image"))
+    brand = str(request.forms.get("brand"))
+    price = str(request.forms.get("price"))
+    cursor.execute("INSERT INTO products (product_name, description, brand, price, category, image) values(%s, %s, %s, %s, %s, %s)", (product_name, description, brand, price, category, image))
+    conn.commit()
+    sql_get_product_id = "SELECT last_value FROM products_product_id_seq"
+    cursor.execute(sql_get_product_id)
+    product_id = cursor.fetchone()
+    supplier = str(request.forms.get("supplier"))
+    product_cost = str(request.forms.get("cost"))
+    quantity = str(request.forms.get("quantity"))
+    cursor.execute("INSERT INTO inventory (product_id, supplier, product_cost, quantity) values(%s, %s, %s, %s)", (product_id, supplier, product_cost, quantity))
+    conn.commit()
+    redirect("/suppliers")
+
+
 @route("/staff")
 def list_staff():
     sql_staff = "SELECT * from staff ORDER BY staff_id ASC"
@@ -69,10 +100,14 @@ def list_staff():
 
 @route("/inventory")
 def list_stock():
+    sql_get_supplier = "SELECT supplier_name FROM supplier"
+    cursor.execute(sql_get_supplier)
+    suppliers = cursor.fetchall()
     sql_get_stock = "SELECT product_name, brand, price, image, supplier, quantity, product_cost, category, products.product_ID FROM (products JOIN inventory ON products.product_id=inventory.product_id) WHERE quantity > 0 ORDER BY product_name ASC"
     cursor.execute(sql_get_stock)
     stock = cursor.fetchall()
-    return template("inventory", stock=stock)
+    return template("inventory", stock=stock, suppliers=suppliers)
+
 
 @route("/out_of_stock")
 def list_out_of_stock():
